386a9de6d097537936f37d6e11fea3a626f32a51
Steve L <slerner@splunk.com>
2020-12-03 13:42:45 -0500

    Create get-logins.sh

---
+++ api-terraform/get-logins.sh
@@ -0,0 +1,6 @@
+curl \
+    --request POST \
+    --header "Content-Type: application/json" \
+    --header "X-SF-Token: YOURTOKENHERE" \
+    --data '{"query":"(sf_eventType:\"SessionLog\")","programToQuery":null,"limit":50,"offset":0,"endTime":1607018740603,"startTime":1604340340601,"orderBy":["-sf_timestamp"],"fields":["*"]}' \
+    https://api.us1.signalfx.com/v2/event/find
